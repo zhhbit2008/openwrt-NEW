625dd3e9a37bcfdbbbc8bad5b13abebe7598a93d
zhhbit2008 <130268627+zhhbit2008@users.noreply.github.com>
2023-06-25 19:15:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+git clone https://git.openwrt.org/feed/packages.git package/packages
+git clone https://git.openwrt.org/project/luci.git package/luci
+git clone https://git.openwrt.org/feed/routing.git package/routing
+git clone https://git.openwrt.org/feed/telephony.git  package/telephony
+git clone https://github.com/kenzok8/openwrt-packages.git  package/kenzo
+git clone https://github.com/kenzok8/small.git package/small
