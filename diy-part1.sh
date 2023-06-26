cd94804990a2a938df564e466f0dd75807ce7f45
zhhbit2008 <130268627+zhhbit2008@users.noreply.github.com>
2023-06-26 18:09:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # echo "src-git packages https://git.openwrt.org/feed/packages.git" >> feeds.conf.default
