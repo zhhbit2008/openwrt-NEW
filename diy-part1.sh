ff33f414762621d7207d6803ff4ac162ac852baf
zhhbit2008 <130268627+zhhbit2008@users.noreply.github.com>
2023-06-26 09:58:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo "src-git packages https://git.openwrt.org/feed/packages" >> "feeds.conf.default"
-echo "src-git luci https://git.openwrt.org/project/luci" >> "feeds.conf.default"
-echo "src-git routing https://git.openwrt.org/feed/routing" >> "feeds.conf.default"
-echo "src-git telephony https://git.openwrt.org/feed/telephony" >> "feeds.conf.default"
-echo "src-git kenzok8 https://github.com/kenzok8/openwrt-packages" >> "feeds.conf.default"
-echo "src-git small https://github.com/kenzok8/small" >> "feeds.conf.default"
+echo "src-git packages https://git.openwrt.org/feed/packages.git" >> "feeds.conf.default"
+echo "src-git luci https://git.openwrt.org/project/luci.git" >> "feeds.conf.default"
+echo "src-git routing https://git.openwrt.org/feed/routing.git" >> "feeds.conf.default"
+echo "src-git telephony https://git.openwrt.org/feed/telephony.git" >> "feeds.conf.default"
+echo "src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git" >> "feeds.conf.default"
+echo "src-git small https://github.com/kenzok8/small.git" >> "feeds.conf.default"
